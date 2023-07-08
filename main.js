3c74ec18c3e7a972237d3333bfaa6de630483776
melkeades <118681335+melkeades@users.noreply.github.com>
2023-07-08 04:42:12 +0300

    clip path

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 import './style.styl'
+import { gsap } from 'gsap'
+import Lenis from '@studio-freight/lenis'
 
-console.log('xxcv')
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+const approachImg = document.querySelector('.approach-sec__img-wrap')
+const approachTitle = document.querySelector('.approach-sec__title-fg')
+// approachTitle.style.opacity = '0.5'
+let approachTitleMask = approachImg.getBoundingClientRect().left - approachTitle.getBoundingClientRect().left
+approachTitle.style.clipPath = 'inset(0 0 -50px ' + approachTitleMask + 'px )'
+// console.log(approachTitle.getBoundingClientRect().left)
+// console.log(approachImg.offsetLeft, approachImg.getBoundingClientRect().left)
