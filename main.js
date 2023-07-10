f48fa17c961105282fbff2e923c5e5c508c36b3a
melkeades <118681335+melkeades@users.noreply.github.com>
2023-07-11 01:34:49 +0300

    mq done

--- main.js
+++ main.js
@@ -13,8 +13,61 @@ requestAnimationFrame(raf)
 
 const approachImg = document.querySelector('.approach-sec__img-wrap')
 const approachTitle = document.querySelector('.approach-sec__title-fg')
-// approachTitle.style.opacity = '0.5'
-let approachTitleMask = approachImg.getBoundingClientRect().left - approachTitle.getBoundingClientRect().left
-// console.log(approachTitle.getBoundingClientRect().left)
-// console.log(approachImg.offsetLeft, approachImg.getBoundingClientRect().left)
-approachTitle.style.clipPath = 'inset(0 0 -50px ' + approachTitleMask + 'px )'
+
+const mqInit = () => {
+  let approachTitleMask = approachImg.getBoundingClientRect().left - approachTitle.getBoundingClientRect().left
+  approachTitle.style.clipPath = 'inset(0 0 -50px ' + approachTitleMask + 'px )'
+}
+mqInit()
+const screen = {
+  mob: 0,
+  mobLand: 479,
+  tab: 768,
+  desk: 992,
+  hd: 1440,
+  uhd: 1920,
+}
+let screenMq = {}
+Object.entries(screen).forEach(([scr, mq], i) => {
+  if (i === 0) {
+    // mobile
+    screenMq[scr] = window.matchMedia(`(max-width: ${Object.values(screen)[i + 1] - 1}px)`)
+  } else if (i === Object.keys(screen).length - 1) {
+    // uhd/4k
+    screenMq[scr] = window.matchMedia(`(min-width: ${mq}px)`)
+  } // the rest
+  else screenMq[scr] = window.matchMedia(`(min-width: ${mq}px) and (max-width: ${Object.values(screen)[i + 1]}px)`)
+})
+
+// media query change events
+for (let [scr, mq] of Object.entries(screenMq)) {
+  mq.addEventListener('change', mqHandler)
+  // console.log('mq added:' + scr)
+}
+
+// media query handler function
+let mqNow = null
+function mqHandler() {
+  for (let [scr, mq] of Object.entries(screenMq)) {
+    if (mq.matches) mqNow = scr
+  }
+  console.log(mqNow)
+}
+mqHandler()
+
+function mqMax(device) {
+  const deviceIndex = Object.keys(screen).indexOf(device)
+  const mqNowIndex = Object.keys(screen).indexOf(mqNow)
+  if (mqNowIndex <= deviceIndex) return true
+  else return false
+}
+function mqMin(device) {
+  const deviceIndex = Object.keys(screen).indexOf(device)
+  const mqNowIndex = Object.keys(screen).indexOf(mqNow)
+  if (mqNowIndex >= deviceIndex) return true
+  else return false
+}
+window.addEventListener('resize', () => {
+  // if (mqMin('tab')) // console.log('qwe')
+  mqInit()
+})
